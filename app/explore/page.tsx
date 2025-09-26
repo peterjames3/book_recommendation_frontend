d0e8e647598d6e9a6e8f99bbe67dac82998aefe4
peterjames3 <kamauchief97@gmail.com>
2025-09-26 13:54:46 +0300

    implementing  the explore page

---
+++ app/explore/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+import MainHero from "../ui/components/explore/hero.main";
+export default function ExplorePage() {
+    return(
+     <>
+     <MainHero />
+     </>
+    )
+}
---
+++ app/ui/components/explore/hero-left.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+
+
+
+
+
+const LeftHero = () => {
+
+
+    return (
+        <div className=" h-full w-full mx-auto  px-6 pt-12">
+            <div className="flex  items-center h-full">
+
+                <div>
+                    <h1 className="text-3xl sm:text-[2.5rem] lg:text-[3rem] xl:text-[4.1rem] font-bold mb-4 leading-tight">
+                        <span className="text-text pompiere-regular">LitKenya </span> -
+                        <span className="text-normalText">We Have Everything your are looking for </span>
+
+                    </h1>
+
+                    <p className="text-normalText mb-4 text-lg leading-relaxed">
+                        Discover millions of books from Open Library. Search by title, author, or browse by genre.
+                        <br />
+                        Search by title, author, or browse by genre.
+                    </p>
+
+                  
+
+                    <div>
+                  
+                    
+                    
+               
+            
+                    </div>
+
+                </div>
+               
+            </div>
+        </div>
+    );
+};
+
+export default LeftHero;
